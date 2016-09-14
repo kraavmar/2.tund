e92b5d086e720b774289b2ca342a8e876af9400d
marikraav <marikraav@greeny.cs.tlu.ee>
2016-09-14 08:59:37 +0300

    first lines of code

---
+++ helloworld.php
@@ -0,0 +1,10 @@
+<?php
+	$firstName = "Mariann";
+	$lastName = "Kraav";
+	
+	// echo trükib välja muutuja väärtuse
+	// echo $firstName;
+	// echo $lastName;
+	
+	echo $firstName." ".$lastName; // . liidab stringe
+?>
