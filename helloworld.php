86828bd90c3b05924caa9906fbb47c73ada38f75
marikraav <marikraav@greeny.cs.tlu.ee>
2016-09-14 09:34:54 +0300

    more lines of code

--- helloworld.php
+++ helloworld.php
@@ -8,3 +8,39 @@
 	
 	echo $firstName." ".$lastName; // . liidab stringe
 ?>
+
+<br>
+
+<?php
+
+	$age = 21;
+	
+	if ($age < 18){
+		// kui vastus on tõene (true)	
+		echo "alaealine";
+	} else {
+		// kui vastus on väär (false)
+		echo "täisealine";
+	}
+
+?>
+
+<br>
+
+<?php
+
+	for ($i = 1; $i <= $age; $i = $i + 1){ //for ($i = 0; $i < $age; $i = $i + 1
+		//echo "palju ";
+		echo $i."palju ";
+	}
+	echo "õnne";
+
+?>
+
+<br>
+
+<?php
+
+	echo date("l, d.F, Y, H.i.s");
+
+?>
